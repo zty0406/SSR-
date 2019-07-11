39e013bcc4baba9508035fbe80a542f01b22fe50
朱文志 <w2971730694@163.com>
2019-07-11 09:45:04 +0800

    项目初始化

---
+++ pages/air/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="air">机票主页</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login">登录页</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
